03ecc310ef624cf0bd185c75095eb8fce3dcd1ab
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-09-12 20:38:04 +0600

    working prototype

--- R/abc_rlda.R
+++ R/abc_rlda.R
@@ -1,43 +1,3 @@
-#' Title
-#'
-#' @param X1 asd
-#' @param X2 asd
-#' @param H asd
-#' @param S asd
-#' @param c asd
-#' @param gamma asd
-#' @param kappa asd
-#'
-#' @return asd
-#' @export
-#'
-#' @examples
-abc_rlda <- function(X1,X2,H,S,c,gamma,kappa) {
-	m1 = rowMeans(X1)
-	m2 = rowMeans(X2)
-	md = m1 - m2
-	ms = m2 + m1
-	at = H%*%md
-	mt = t(at)%*%ms
-	# Mat = t(at)%*%sig.true%*%at
-	m = -0.5*mt - log((1-c)/c)/kappa
-	p = dim(X1)[1]
-	n1 = dim(X1)[2]
-	n2 = dim(X2)[2]
-	trH = sum(diag(H))
-	s = 1-p/(n1+n2-2)+trH/(n1+n2-2)
-	deltah = (p/(n1+n2-2)-trH/(n1+n2-2))/(gamma*s)
-	G1 = 0.5*t(m1-m2)%*%H%*%md - ((n1+n2-2)/n1) * deltah - log((1-c)/c)/kappa
-	G2 = 0.5*t(m2-m1)%*%H%*%md + ((n1+n2-2)/n2) * deltah - log((1-c)/c)/kappa
-	D = t(at)%*%S%*%at
-	D = D*(1+gamma*deltah)^2
-	omegaopt = gamma*(D*log((1-c)/c)/(G2-G1)-0.5*(G1+G2))
-  m = m + omegaopt/gamma #add the bias term
-	av = c(m,at)
-	return(av)
-}
-
-
 # ----------------- Parameter estimation -----------------------
 grid_search <- function(range_gamma, range_C_01, range_C_10){
 
@@ -57,8 +17,21 @@ train <- function(){
 
 # ----------------------------------------------------------------
 
-new_abcrlda <- function(x0, x1, gamma, cost_01, cost_10){
+#' Title
+#'
+#' @param x0
+#' @param x1
+#' @param gamma
+#' @param cost_10
+#'
+#' @return
+#' @export
+#'
+#' @examples
+abcrlda <- function(x0, x1, gamma, cost_10){  # cost_01 = 1 -  cost_10
+  kappa <- 1 # by default for now
 
+  c <- cost_10
   X0 = t(as.matrix(x0))
   X1 = t(as.matrix(x1))
   S0 = cov(t(X0))
@@ -70,63 +43,53 @@ new_abcrlda <- function(x0, x1, gamma, cost_01, cost_10){
   S = ((n0-1)*S0+(n1-1)*S1)/(n0+n1-2)
   Hinv = (diag(dim(X1)[1])+gamma*S)
   H = solve(Hinv)
-}
+  # ------ -- - - -- - - - - -
+  m1 = rowMeans(X0)
+  m2 = rowMeans(X1)
+  md = m1 - m2
+  ms = m2 + m1
+  at = H%*%md
+  mt = t(at)%*%ms
+  # Mat = t(at)%*%sig.true%*%at
+  m = -0.5*mt - log((1-c)/c)/kappa
+  p = dim(X0)[1]
+  n1 = dim(X0)[2]
+  n2 = dim(X1)[2]
+  trH = sum(diag(H))
+  s = 1-p/(n1+n2-2)+trH/(n1+n2-2)
+  deltah = (p/(n1+n2-2)-trH/(n1+n2-2))/(gamma*s)
+  G1 = 0.5*t(m1-m2)%*%H%*%md - ((n1+n2-2)/n1) * deltah - log((1-c)/c)/kappa
+  G2 = 0.5*t(m2-m1)%*%H%*%md + ((n1+n2-2)/n2) * deltah - log((1-c)/c)/kappa
+  D = t(at)%*%S%*%at
+  D = D*(1+gamma*deltah)^2
+  omegaopt = gamma*(D*log((1-c)/c)/(G2-G1)-0.5*(G1+G2))
+  m = as.numeric(m + omegaopt/gamma) #add the bias term
+  return(structure(list(m=m,a=at,cost=c), class="abcrlda"))
 
-# ----------------------------------------------------------------
-# predicts group membership for new data x and object from abcrlda
-predict.abcrlda <- function(object, x, ...){
-  ## check requirements
-  if(class(object) != "abcrlda")
-    stop("object has to be of type abcrlda")
-
-  ## get dimension
-  p = ncol(object$means)
-
-  if(!is.vector(x) && !is.matrix(x) && !is.data.frame(x))
-    stop("'x' has to be a vector, matrix or data.frame")
-
-  n <- 0
-  if(is.vector(x)){
-    if(length(x) != p)
-      stop("'x' has to be of length ", p)
-
-    y <- NULL
-    for(i in 1:length(x))
-      y <- cbind(y, x[i])
-    x <- y
-    n <- 1
-  }else{
-    if(ncol(x) != p)
-      stop("'x' has to be of dimension ", p)
-
-    x <- as.matrix(x)
-    n <- nrow(x)
-  }
-
-  probs <- t(-2*object$means %*% object$covi %*% t(x) + diag(object$means %*% object$covi %*% t(object$means)) - 2*log(object$prior[[1]]))
-  cl = object$lev[apply(probs, 1, which.min)]
-
-  ret = list(class=cl, posterior=probs)
-  ret
 }
 
 #' Title
 #'
-#' @param X
-#' @param a
-#' @param m
-#' @param c
+#' @param object
+#' @param x
 #'
 #' @return
 #' @export
 #'
 #' @examples
-test_lda<-function(X,a,m,c) {
-  l = X %*% a
-  if ((l + m) > c) {
-    d = 1
-  }else{
-    d = 2
-  }
-  return(d)
+predict_abcrlda <- function(object, x){
+  ## check requirements
+  if(class(object) != "abcrlda")
+    stop("object has to be of type abcrlda")
+
+  if(!is.vector(x) && !is.matrix(x) && !is.data.frame(x)) #  && !is.data.frame(x)
+    stop("'x' has to be a vector, matrix or data.frame")
+
+  if(!is.data.frame(x))
+    x = as.matrix(x)
+
+
+  l <- x %*% object$a + object$m
+  return(as.numeric(l > object$cost))
+
 }
