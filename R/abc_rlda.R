db07b86437ca39dfef022e732181513f3df393de
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-09-13 16:30:19 +0600

    optimized abcrlda cross checked it with equations from paper and added comments

--- R/abc_rlda.R
+++ R/abc_rlda.R
@@ -31,40 +31,38 @@ train <- function(){
 abcrlda <- function(x0, x1, gamma, cost_10){  # cost_01 = 1 -  cost_10
   kappa <- 1 # by default for now
 
-  c <- cost_10
+  # p = dim(X0)[1]
+  p = ncol(x0)  # number of dimensions
+  n0 = nrow(x0) # number of samples in x0
+  n1 = nrow(x1) # number of samples in x1
   X0 = t(as.matrix(x0))
   X1 = t(as.matrix(x1))
   S0 = cov(t(X0))
   S1 = cov(t(X1))
-
-  n0 = nrow(x0)
-  n1 = nrow(x1)
-
   S = ((n0-1)*S0+(n1-1)*S1)/(n0+n1-2)
   Hinv = (diag(dim(X1)[1])+gamma*S)
   H = solve(Hinv)
   # ------ -- - - -- - - - - -
   m1 = rowMeans(X0)
   m2 = rowMeans(X1)
-  md = m1 - m2
-  ms = m2 + m1
-  at = H%*%md
-  mt = t(at)%*%ms
+  mdif = m1 - m2
+  msum = m2 + m1
+  at = H %*% mdif   # why coefs are here?
+
+  mt = t(at) %*% msum  # should be scalar
   # Mat = t(at)%*%sig.true%*%at
-  m = -0.5*mt - log((1-c)/c)/kappa
-  p = dim(X0)[1]
-  n1 = dim(X0)[2]
-  n2 = dim(X1)[2]
+  m = -0.5*mt - log((1-cost_10)/cost_10)/kappa
+  # ------- omega optimal calculation -------------
   trH = sum(diag(H))
-  s = 1-p/(n1+n2-2)+trH/(n1+n2-2)
-  deltah = (p/(n1+n2-2)-trH/(n1+n2-2))/(gamma*s)
-  G1 = 0.5*t(m1-m2)%*%H%*%md - ((n1+n2-2)/n1) * deltah - log((1-c)/c)/kappa
-  G2 = 0.5*t(m2-m1)%*%H%*%md + ((n1+n2-2)/n2) * deltah - log((1-c)/c)/kappa
+  s = 1-p/(n0+n1-2)+trH/(n0+n1-2)
+  deltah = (p/(n0+n1-2)-trH/(n0+n1-2))/(gamma*s)
+  G1 = 0.5*t(m1-m2)%*%H%*%mdif - ((n0+n1-2)/n0) * deltah - log((1-cost_10)/cost_10)/kappa
+  G2 = 0.5*t(m2-m1)%*%H%*%mdif + ((n0+n1-2)/n1) * deltah - log((1-cost_10)/cost_10)/kappa
   D = t(at)%*%S%*%at
   D = D*(1+gamma*deltah)^2
-  omegaopt = gamma*(D*log((1-c)/c)/(G2-G1)-0.5*(G1+G2))
+  omegaopt = gamma*(D*log((1-cost_10)/cost_10)/(G2-G1)-0.5*(G1+G2))
   m = as.numeric(m + omegaopt/gamma) #add the bias term
-  return(structure(list(m=m,a=at,cost=c), class="abcrlda"))
+  return(structure(list(m=m,a=at,cost=cost_10), class="abcrlda"))
 
 }
 
