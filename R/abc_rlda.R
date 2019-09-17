1fedeae54f64e89326fd4316b04fb6069b669094
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-09-17 13:37:14 +0600

    Risk estimation using formula and using cross validation

--- R/abc_rlda.R
+++ R/abc_rlda.R
@@ -1,18 +1,69 @@
 # ----------------- Parameter estimation -----------------------
-grid_search <- function(range_gamma, range_C_01, range_C_10){
+#' Title
+#'
+#' @param x0
+#' @param x1
+#' @param range_gamma
+#' @param range_C_10
+#' @param method
+#'
+#' @return
+#' @export
+#'
+#' @examples
+grid_search <- function(x, grouping, range_gamma, range_C_10, method="estimator",
+                        k_fold=3){
 
+  list_gamma = numeric()
+  list_C_10 = numeric()
+  list_estimates = numeric()
+  if(method == "estimator"){
+    for(gamma in range_gamma){
+      for(C_10 in range_C_10){
+        abcrlda_model <- abcrlda(x, grouping, gamma, C_10)
+        list_gamma = c(list_gamma, gamma)
+        list_C_10 = c(list_C_10, C_10)
+        list_estimates = c(list_estimates, risk_estimate_20(abcrlda_model))
+      }
+    }
+  }else if(method == "cross"){
+    for(gamma in range_gamma){
+      for(C_10 in range_C_10){
+        list_gamma = c(list_gamma, gamma)
+        list_C_10 = c(list_C_10, C_10)
+        list_estimates = c(list_estimates, cross_validation(x, grouping, gamma, C_10))
+      }
+    }
+  }
+  best_param_index <- which(list_estimates == min(list_estimates))
+  return(structure(list(gamma = list_gamma[best_param_index],
+                        C_10 = list_C_10[best_param_index],
+                        e = list_estimates[best_param_index]
+                        )))
 }
 
-cross_validation <- function(gamma, C_01, C_10){
+cross_validation <- function(x, grouping, gamma, C_10, kfolds=10){
+  shufled_index = sample(nrow(x))
+  x <- x[shufled_index,]
+  grouping <- grouping[shufled_index,]
 
-}
+  folds <- cut(seq(1,nrow(x)),breaks=kfolds,labels=FALSE)
+  e_cross <- numeric()
 
-equation29 <- function(gamma, C_01, C_10){
-
-}
-
-train <- function(){
-
+  for(i in 1:kfolds){
+    #Segement your data by fold using the which() function
+    testIndexes <- which(folds==i, arr.ind=TRUE)
+    testData <- x[testIndexes, ]
+    testLabel <- grouping[testIndexes, ]
+    trainData <- x[-testIndexes, ]
+    trainLabel <- grouping[-testIndexes, ]
+    #Use the test and train data partitions however you desire...
+    abcrlda_model <- abcrlda(trainData, trainLabel, gamma, C_10)
+    predictions <- predict_abcrlda(abcrlda_model, testData)
+    error <- sum(predictions != testLabel) / length(testLabel)
+    e_cross <- c(e_cross, error)
+  }
+  return(mean(e_cross))
 }
 
 # ----------------------------------------------------------------
@@ -28,7 +79,10 @@ train <- function(){
 #' @export
 #'
 #' @examples
-abcrlda <- function(x0, x1, gamma, cost_10, kappa = 1){  # cost_01 = 1 -  cost_10
+abcrlda <- function(x, grouping, gamma, cost_10 = 0.5, kappa = 1){  # cost_01 = 1 -  cost_10
+
+  x0 <- x[grouping == 0,]
+  x1 <- x[grouping == 1,]
   p = ncol(x0)  # number of dimensions
   n0 = nrow(x0) # number of samples in x0
   n1 = nrow(x1) # number of samples in x1
@@ -55,10 +109,18 @@ abcrlda <- function(x0, x1, gamma, cost_10, kappa = 1){  # cost_01 = 1 -  cost_1
   Ghat0 = G0 - ((n0+n1-2)/n0) * deltahat
   Ghat1 = G1 + ((n0+n1-2)/n1) * deltahat
   D = t(at) %*% S %*% at
+  # D = t(mdif) %*% H %*% S %*% H %*% mdif  # no difference, more explicit
   Dhat = D*(1+gamma*deltahat)^2
   omegaopt = gamma * (Dhat*log((1-cost_10)/cost_10)/(Ghat1-Ghat0) - 0.5*(Ghat0+Ghat1))
-  m = as.numeric(m + omegaopt/gamma) # add the bias term
-  return(structure(list(a=at, m=m, cost=cost_10), class="abcrlda"))
+  m = as.numeric(m + omegaopt/gamma) # add the bias term, ? should it be kappa
+  return(structure(list(a = at,
+                        m = m,
+                        cost_10 = cost_10,
+                        gamma = gamma,
+                        Ghat0 = Ghat0,
+                        Ghat1 = Ghat1,
+                        Dhat = Dhat,
+                        omegaopt = omegaopt), class="abcrlda"))
 
 }
 
@@ -82,6 +144,46 @@ predict_abcrlda <- function(object, x){
   if(!is.data.frame(x))
     x = as.matrix(x)
 
-  return(as.numeric(x %*% object$a + object$m <= object$cost))
+  return(as.numeric(x %*% object$a + object$m <= 0))  # object$cost
 
 }
+
+#' Title
+#'
+#' @param object
+#'
+#' @return
+#' @export
+#'
+#' @examples
+risk_estimate_20 <- function(object){
+  ## check requirements
+  if(class(object) != "abcrlda")
+    stop("object has to be of type abcrlda")
+
+  e0 = error_estimate_29(object, 0)
+  e1 = error_estimate_29(object, 1)
+  return(object$cost_10 * e0 + (1 - object$cost_10) * e1)
+ }
+
+
+#' Title
+#'
+#' @param object
+#' @param i
+#'
+#' @return
+#' @export
+#'
+#' @examples
+error_estimate_29 <- function(object, i){
+  ## check requirements
+  if(class(object) != "abcrlda")
+    stop("object has to be of type abcrlda")
+
+  Ghat = c(object$Ghat0, object$Ghat1)
+  X = (-1)^i * (Ghat[1+i] + object$omegaopt/object$gamma) / sqrt(object$Dhat)
+  return(1 - pnorm(X))
+}
+
+
