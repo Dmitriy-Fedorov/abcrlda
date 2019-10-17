c08b38841da7977d1b47e80e7dd1920bf5c3f720
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-10-17 17:11:12 +0600

    cross validation bug fix for high values of nfolds

--- R/abc_grid.R
+++ R/abc_grid.R
@@ -1,6 +1,6 @@
 
 #' Grid Search
-#' @description Performs grid search based on cross validation or error estimation formula.
+#' @description Performs grid search based on double asymptotic risk estimation or cross validation.
 #'
 # @param x Matrix or data.frame of observations.
 # @param y Grouping variable. A vector of numeric values 0 and 1 is recommended.
@@ -33,7 +33,7 @@ grid_search <- function(x, y, range_gamma, range_cost,
         cost <- range_cost[i, ]
         abcrlda_model <- abcrlda(x, y, gamma, cost)
         list_gamma <- c(list_gamma, gamma)
-        list_cost <- c(list_cost, cost)
+        list_cost <- rbind(list_cost, cost)
         list_estimates <- c(list_estimates, da_risk_estimator(abcrlda_model))
       }
     }
@@ -42,19 +42,20 @@ grid_search <- function(x, y, range_gamma, range_cost,
       for (i in 1:nrow(range_cost)){
         cost <- range_cost[i, ]
         list_gamma <- c(list_gamma, gamma)
-        list_cost <- c(list_cost, cost)
+        list_cost <- rbind(list_cost, cost)
         list_estimates <- c(list_estimates,
                             cross_validation(x, y, gamma, cost, nfolds))
       }
     }
   }
-  best_param_index <- which(list_estimates == min(list_estimates))
+  best_param_index <- list_estimates == min(list_estimates)
   return(structure(list(gamma = list_gamma[best_param_index],
-                        cost = list_cost[best_param_index],
+                        cost = list_cost[best_param_index, ],
                         e = list_estimates[best_param_index])))
 }
 
 #' Cross Validation
+#' @description
 #' @inheritParams grid_search
 #' @inheritParams abcrlda
 # @param gamma regularization parameter
@@ -62,18 +63,22 @@ grid_search <- function(x, y, range_gamma, range_cost,
 # It's value should be between 0 and 1 (0 < cost_10 < 1)
 # Values bigger than 0.5 prioretizes correct classification of 0 class while values less than 0.5 prioretizes 1 class
 # @param nfolds Number of for cross validation algorithm
-#' @return Returns average error of cross validation
+#' @return Returns average risk after cross validation
 #' @export
 #' @family functions in the package
 #'
 #' @example inst/examples/example_cross.R
 cross_validation <- function(x, y, gamma=1, cost=c(0.5, 0.5), nfolds=10){
 
+  x <- as.matrix(x)
   shufled_index <- sample(nrow(x))
   x <- x[shufled_index, ]
   y <- y[shufled_index]
+
   folds <- cut(seq(1, nrow(x)), breaks = nfolds, labels = FALSE)
   e_cross <- numeric()
+  e_e0 <- numeric()
+  e_e1 <- numeric()
 
   if (!is.factor(y))
     y <- as.factor(y)
@@ -89,47 +94,84 @@ cross_validation <- function(x, y, gamma=1, cost=c(0.5, 0.5), nfolds=10){
            cost should be between 0 and 1 (not including)")
     cost <- c(cost, 1 - cost)
   }
+
   if (length(cost) != 2)
     stop("cost vector should be of length 1 or 2, this is binary classifier")
+
+  if (nfolds < 3)
+    stop("nfolds must be bigger than 3; nfolds=10 recommended")
+
+  if (nfolds > nrow(x))
+    stop("number of folds is greater than number of samples")
 
   for (i in 1:nfolds){
     # Segement your data by fold using the which() function
     test_indexes <- which(folds == i, arr.ind = TRUE)
-    test_data <- x[test_indexes, ]
+    test_data <- x[test_indexes, , drop = FALSE]
     test_label <- y[test_indexes]
-    train_data <- x[-test_indexes, ]
+    train_data <- x[-test_indexes, , drop = FALSE]
     train_label <- y[-test_indexes]
 
     abcrlda_model <- abcrlda(train_data, train_label, gamma, cost)
-    # print(test_label)
-    test0 <- test_data[test_label == lev[1], ]
-    test1 <- test_data[test_label == lev[2], ]
-    # print(test0)
-    # print(test1)
-    res0 <- stats::predict(abcrlda_model, test0, type = "raw") - 1
-    res1 <- stats::predict(abcrlda_model, test1, type = "raw") - 1
-    # print(res0)
-    # print(res1)
-    nerr0 <- sum(res0)
-    nerr1 <- sum(!res1)
-    # print(c(nerr0, nerr1))
-    err0 <- nerr0 / length(res0)
-    err1 <- nerr1 / length(res1)
+    # print(abcrlda_model$a)
+    # print(abcrlda_model$m)
+    # print(dim(train_data))
+    # print(dim(test_data))
+    test0 <- test_data[test_label == lev[1], , drop = FALSE]
+    test1 <- test_data[test_label == lev[2], , drop = FALSE]
+    # print(paste("0", nrow(test0), test0))
+    # print(paste("1", nrow(test1), test1))
+    # print("______")
 
-    error <- err0 * cost[1] + err1 * cost[2]
-    e_cross <- c(e_cross, error)
+    if (nrow(test0) > 0){
+      # res0 <- as.numeric(stats::predict(abcrlda_model, test0)) - 1
+      res0 <- as.numeric(test0 %*% abcrlda_model$a + abcrlda_model$m <= 0)
+      nerr0 <- sum(res0)
+      # if(res0 == 1){
+      #   print(res0 == 0)
+      #   print(res0)
+      #   print("0 ______ ")
+      # }
+      err0 <- nerr0 / length(res0)
+      e_e0 <- c(e_e0, err0)
+    }else{
+      err0 <- 0
+    }
+    if (nrow(test1) > 0){
+      # res1 <- as.numeric(stats::predict(abcrlda_model, test1)) - 1
+      res1 <- as.numeric(test1 %*% abcrlda_model$a + abcrlda_model$m <= 0)
+      nerr1 <- sum(!res1)
+      err1 <- nerr1 / length(res1)
+      e_e1 <- c(e_e1, err1)
+    }else{
+      err1 <- 0
+    }
+
+    risk <- err0 * cost[1] + err1 * cost[2]
+    e_cross <- c(e_cross, risk)
+
+
+
   }
   # print(e_cross)
-  return(mean(e_cross))
+  return(list(e_cross = mean(e_cross),
+              ecr = mean(e_e0) * cost[1] + mean(e_e1) * cost[2],
+              e0 = mean(e_e0),
+              e1 = mean(e_e1)))
 }
 
 
-# da_risk_estimator double asymptotic
+# cross_validation <- function(x, y, gamma=1, cost=c(0.5, 0.5), nfolds=10){
+#
+#
+# }
+
 #' Double Asymptotic Risk Estimator
-#' @description Calculates weighted error based on normalized cost values
+#' @description Generalized consistent estimator of risk
 #' @inheritParams predict.abcrlda
 #'
-#' @return Weighted error based on "abcrlda" object
+#' @return Calculates risk based on estimated class error rates and misclassification costs
+#'   \deqn{\Re = \varepsilon_0 * cost_{10} + \varepsilon_1 * cost_{01}}{R = e0 * cost_10 + e1 * cost_01)}
 #' @export
 #' @family functions in the package
 #' @example inst/examples/example_risk.R
@@ -150,3 +192,4 @@ da_risk_estimator <- function(object){
   e1 <- error_estimate_29(object, 1)
   return(object$cost[1] * e0 + object$cost[2] * e1)
 }
+
